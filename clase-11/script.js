a333b6065893642acfeb9c693b30e17535b449a7
Lucas Nagel <lucasanagel@gmail.com>
2024-05-06 22:11:50 -0300

    clase 11

---
+++ clase-11/script.js
@@ -0,0 +1,141 @@
+/* tipos de datos */
+
+/* datos objeto: proximamente */
+
+/* 
+
+datos primitivos
+
+-string
+-number
+
+-null (ausencia o nulidad de un dato)
+-undifined (no hay un dato definido)
+-boolean (true o false)
+
+*/
+
+/* 
+
+llego a la casa y me atienden : true o false
+
+me pierdo en el camino : undefined
+
+llego a la casa y no me atienden : null
+
+*/
+
+
+
+/* operadores aritmeticos */
+
+
+
+/*
+
+siempre devuelve string
+
++: concatenacion 
+*/
+
+
+
+
+/*
+siempre devuelve numeros
+
++: suma
+
+-: resta
+
+*: multiplicacion
+
+/: division
+
+%: sacar resto de la division
+
+*/
+
+
+
+
+/*
+QUE PASA SI:
+
+
+"hola" + 1 = "hola1"
+
+proceso interno: "hola" + String(1) = hola1
+
+
+1 + true
+
+proceso interno: 1 + Number(true) = 2
+resulta: 1 + 1 = 2
+
+
+NaN + cualquier dato = NaN
+
+
+Number(undefined) = NaN
+
+
+Number(null) = 0
+
+
+null + true = 1
+
+*/
+
+
+
+/* 
+para comprobar algun tipo de dato:
+
+typeof(1)
+
+Number
+*/
+
+
+
+/* Falsy o Truthy */
+
+/* valores falsos
+0
+""
+false
+null
+undefined
+NaN
+*/
+
+/* 
+Boolean(1)
+true
+
+Boolean("")
+false
+*/
+
+
+/* Funciones nativas
+
+typeof() tipo de dato de ()
+isNaN() devuelve boolean dependiendo si () es NaN o no
+
+*/
+
+
+/* Comparadores 
+
+== igual
+=== estricta igualdad
+!= diferencia
+!== estricta diferencia
+< menor que
+> mayor que
+>= mayor o igual a
+<= menor o igual a
+
+*/

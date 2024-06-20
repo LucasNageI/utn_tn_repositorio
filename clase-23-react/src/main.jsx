8e87309b14437953e42bbd3b895bd5742f48783e
LucasNagel <lucasanagel@gmail.com>
2024-06-19 21:58:37 -0300

    clase-23-react

---
+++ clase-23-react/src/main.jsx
@@ -0,0 +1,13 @@
+/* todos los archivos jsx deben importar react */
+import React from 'react' 
+
+/* se usa reactDOM para crear el dom virtual (de react) */
+import ReactDOM from 'react-dom/client'
+
+/* importa el componente App */
+/* un componente es una funcion que retorna JSX (es un codigo parecido a HTML) */
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App /> /* esta es la invocacion de un componente */
+)
